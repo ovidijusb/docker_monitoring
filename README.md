c93edde5b587ddd36f72153ea11f236fb2b2d125
Ovidijus Boiko <ovidijus@gmail.com>
2019-06-18 12:50:50 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# vinted-monitoring
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: vinted-monitoring
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
